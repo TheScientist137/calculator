8cdf497aa02dce83f9dcb366a70bb03d198e1aa0
The-Scientist <thescientist137@outlook.es>
2023-11-02 16:38:44 +0100

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,196 @@
 import { useState } from 'react'
+import './App.css'
 
 function App () {
-  const [display, setDisplay] = useState('0')
+  const [answer, setAnswer] = useState('')
+  const [expression, setExpression] = useState('')
+  const et = expression.trim()
 
-  const handleNumber = event => {
-    const number = event.target.textContent
+  const isOperator = symbol => {
+    return /[*/+-]/.test(symbol)
+  }
 
-    if (display === '0') {
-      setDisplay(number)
+  const buttonPress = symbol => {
+    if (symbol === 'clear') {
+      setAnswer('')
+      setExpression('0')
+    } else if (symbol === 'negative') {
+      if (answer === '') return
+      setAnswer(
+        answer.toString().charAt(0) === '-' ? answer.slice(1) : '-' + answer
+      )
+    } else if (symbol === 'percent') {
+      if (answer === '') return
+      setAnswer((parseFloat(answer) / 100).toString())
+    } else if (isOperator(symbol)) {
+      setExpression(et + ' ' + symbol + ' ')
+    } else if (symbol === '=') {
+      calculate()
+    } else if (symbol === '0') {
+      if (expression.charAt(0) !== '0') {
+        setExpression(expression + symbol)
+      }
+    } else if (symbol === '.') {
+      const lastNumber = expression.split(/[-+/*]/g).pop()
+      if (!lastNumber) return
+      console.log('lastNumber :>> ', lastNumber)
+      if (lastNumber?.includes('.')) return
+      setExpression(expression + symbol)
     } else {
-      setDisplay(display + number)
+      if (expression.charAt(0) === '0') {
+        setExpression(expression.slice(1) + symbol)
+      } else {
+        setExpression(expression + symbol)
+      }
     }
+  }
+
+  const calculate = () => {
+    if (isOperator(et.charAt(et.length - 1))) return
+
+    const parts = et.split(' ')
+    const newParts = []
+
+    for (let i = parts.length - 1; i >= 0; i--) {
+      if (['*', '/', '+'].includes(parts[i]) && isOperator(parts[i - 1])) {
+        newParts.unshift(parts[i])
+        let j = 0
+        let k = i - 1
+        while (isOperator(parts[k])) {
+          k--
+          j++
+        }
+        i -= j
+      } else {
+        newParts.unshift(parts[i])
+      }
+    }
+    const newExpression = newParts.join(' ')
+    if (isOperator(newExpression.charAt(0))) {
+      setAnswer(eval(answer + newExpression).toString())
+    } else {
+      setAnswer(eval(newExpression).toString())
+    }
+    setExpression('')
   }
 
   return (
     <div className='container'>
-      <div className='container w-50'>
-        <h1 className='text-center mb-5'>Calculator</h1>
-        <div id='display' className='display bg-light border p-2 mb-2'>
-          {display}
+      <h1>Calculator Application</h1>
+      <div id='calculator'>
+        <div id='display' style={{ textAlign: 'right' }}>
+          <div id='answer'>{answer}</div>
+          <div id='expression'>{expression}</div>
         </div>
-        <div className='d-flex flex  align-items-center justify-content-center h-100 border'>
-          <div id='numbers' className='d-flex flex-wrap justify-content-center'>
-            <button
-              id='seven'
-              className='btn btn-secondary btn-lg w-25  m-2'
-              onClick={handleNumber}
-            >
-              7
-            </button>
-            <button id='eight' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
-              8
-            </button>
-            <button id='nine' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
-              9
-            </button>
-            <button id='four' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
-              4
-            </button>
-            <button id='five' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
-              5
-            </button>
-            <button id='six' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
-              6
-            </button>
-            <button id='one' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
-              1
-            </button>
-            <button id='two' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
-              2
-            </button>
-            <button id='three' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
-              3
-            </button>
-            <button id='zero' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
-              0
-            </button>
-            <button id='decimal' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
-              .
-            </button>
-            <button id='equals' className='btn btn-success btn-lg w-25  m-2' onClick={handleNumber}>
-              =
-            </button>
-          </div>
-          <div
-            id='operators'
-            className='w-75  d-flex flex-column align-items-center'
-          >
-            <button id='clear' className='btn btn-danger btn w-50 m-2'>
-              AC
-            </button>
-            <button id='divide' className='btn btn-primary btn w-50  m-2'>
-              /
-            </button>
-            <button id='multiply' className='btn btn-primary btn w-50  m-2'>
-              x
-            </button>
-            <button id='subtract' className='btn btn-primary btn w-50  m-2'>
-              -
-            </button>
-            <button id='add' className='btn btn-primary btn w-50  m-2'>
-              +
-            </button>
-          </div>
-        </div>
+        <button
+          id='clear'
+          onClick={() => buttonPress('clear')}
+          className='light-gray'
+        >
+          C
+        </button>
+        <button
+          id='negative'
+          onClick={() => buttonPress('negative')}
+          className='light-gray'
+        >
+          +/-
+        </button>
+        <button
+          id='percentage'
+          onClick={() => buttonPress('percent')}
+          className='light-gray'
+        >
+          %
+        </button>
+        <button id='divide' onClick={() => buttonPress('/')} className='yellow'>
+          /
+        </button>
+        <button
+          id='seven'
+          onClick={() => buttonPress('7')}
+          className='dark-gray'
+        >
+          7
+        </button>
+        <button
+          id='eight'
+          onClick={() => buttonPress('8')}
+          className='dark-gray'
+        >
+          8
+        </button>
+        <button
+          id='nine'
+          onClick={() => buttonPress('9')}
+          className='dark-gray'
+        >
+          9
+        </button>
+        <button
+          id='multiply'
+          onClick={() => buttonPress('*')}
+          className='yellow'
+        >
+          *
+        </button>
+        <button
+          id='four'
+          onClick={() => buttonPress('4')}
+          className='dark-gray'
+        >
+          4
+        </button>
+        <button
+          id='five'
+          onClick={() => buttonPress('5')}
+          className='dark-gray'
+        >
+          5
+        </button>
+        <button id='six' onClick={() => buttonPress('6')} className='dark-gray'>
+          6
+        </button>
+        <button
+          id='subtract'
+          onClick={() => buttonPress('-')}
+          className='yellow'
+        >
+          -
+        </button>
+        <button id='one' onClick={() => buttonPress('1')} className='dark-gray'>
+          1
+        </button>
+        <button id='two' onClick={() => buttonPress('2')} className='dark-gray'>
+          2
+        </button>
+        <button
+          id='three'
+          onClick={() => buttonPress('3')}
+          className='dark-gray'
+        >
+          3
+        </button>
+        <button id='add' onClick={() => buttonPress('+')} className='yellow'>
+          +
+        </button>
+        <button
+          id='zero'
+          onClick={() => buttonPress('0')}
+          className='dark-gray'
+        >
+          0
+        </button>
+        <button
+          id='decimal'
+          onClick={() => buttonPress('.')}
+          className='dark-gray'
+        >
+          .
+        </button>
+        <button id='equals' onClick={() => buttonPress('=')} className='yellow'>
+          =
+        </button>
       </div>
     </div>
   )
