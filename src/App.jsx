14634bd4a58f8e93a59957f40d19d31c887a7520
The-Scientist <thescientist137@outlook.es>
2023-11-02 13:10:26 +0100

    remake application version 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,199 +1,88 @@
 import { useState } from 'react'
 
 function App () {
-  const [displayValue, setDisplayValue] = useState('0')
-  const [prevValue, setPrevValue] = useState('')
-  const [operator, setOperator] = useState('')
-  const [waitingForOperand, setWaitingForOperand] = useState(false)
+  const [display, setDisplay] = useState('0')
 
-  const handleNumberClick = digit => {
-    if (waitingForOperand) {
-      setDisplayValue(String(digit))
-      setWaitingForOperand(false)
+  const handleNumber = event => {
+    const number = event.target.textContent
+
+    if (display === '0') {
+      setDisplay(number)
     } else {
-      setDisplayValue((displayValue === '0' ? '' : displayValue) + digit)
+      setDisplay(display + number)
     }
-  }
-
-  const handleOperatorClick = nextOperator => {
-    if (prevValue && operator && !waitingForOperand) {
-      calculate()
-    } else {
-      setPrevValue(displayValue)
-    }
-    setWaitingForOperand(true)
-    setOperator(nextOperator)
-  }
-
-  const handleDecimalClick = () => {
-    if (waitingForOperand) {
-      setDisplayValue('0.') // Si esperas un operando, establece el valor como '0.'
-      setWaitingForOperand(false)
-    } else if (!displayValue.includes('.')) {
-      setDisplayValue(displayValue + '.') // Agregar el punto solo si no está presente
-    }
-  }
-
-  const calculate = () => {
-    const prev = parseFloat(prevValue)
-    const next = parseFloat(displayValue)
-    let result = 0
-
-    if (operator === '+') {
-      result = prev + next
-    } else if (operator === '-') {
-      result = prev - next
-    } else if (operator === 'x') {
-      result = prev * next
-    } else if (operator === '/') {
-      result = prev / next
-    }
-
-    setDisplayValue(result.toString())
-    setPrevValue('')
-    setOperator('')
-    setWaitingForOperand(false)
-  }
-
-  const clear = () => {
-    setDisplayValue('0')
-    setPrevValue('')
-    setOperator('')
-    setWaitingForOperand(false)
   }
 
   return (
-    <div className='container w-50'>
-      <h1 className='text-center mb-5'>Calculator</h1>
-      <div id='display' className='display bg-light border p-2 mb-2'>
-        {displayValue}
-      </div>
-      <div className='d-flex flex  align-items-center justify-content-center h-100 border'>
-        <div id='numbers' className='d-flex flex-wrap justify-content-center'>
-          <button
-            id='seven'
-            className='btn btn-secondary btn-lg w-25  m-2'
-            onClick={() => handleNumberClick(7)}
-          >
-            7
-          </button>
-          <button
-            id='eight'
-            className='btn btn-secondary btn-lg w-25  m-2'
-            onClick={() => handleNumberClick(8)}
-          >
-            8
-          </button>
-          <button
-            id='nine'
-            className='btn btn-secondary btn-lg w-25  m-2'
-            onClick={() => handleNumberClick(9)}
-          >
-            9
-          </button>
-          <button
-            id='four'
-            className='btn btn-secondary btn-lg w-25  m-2'
-            onClick={() => handleNumberClick(4)}
-          >
-            4
-          </button>
-          <button
-            id='five'
-            className='btn btn-secondary btn-lg w-25  m-2'
-            onClick={() => handleNumberClick(5)}
-          >
-            5
-          </button>
-          <button
-            id='six'
-            className='btn btn-secondary btn-lg w-25  m-2'
-            onClick={() => handleNumberClick(6)}
-          >
-            6
-          </button>
-          <button
-            id='one'
-            className='btn btn-secondary btn-lg w-25  m-2'
-            onClick={() => handleNumberClick(1)}
-          >
-            1
-          </button>
-          <button
-            id='two'
-            className='btn btn-secondary btn-lg w-25  m-2'
-            onClick={() => handleNumberClick(2)}
-          >
-            2
-          </button>
-          <button
-            id='three'
-            className='btn btn-secondary btn-lg w-25  m-2'
-            onClick={() => handleNumberClick(3)}
-          >
-            3
-          </button>
-          <button
-            id='zero'
-            className='btn btn-secondary btn-lg w-25  m-2'
-            onClick={() => handleNumberClick(0)}
-          >
-            0
-          </button>
-          <button
-            id='decimal'
-            className='btn btn-secondary btn-lg w-25  m-2'
-            onClick={handleDecimalClick}
-          >
-            .
-          </button>
-          <button
-            id='equals'
-            className='btn btn-success btn-lg w-25  m-2'
-            onClick={calculate}
-          >
-            =
-          </button>
+    <div className='container'>
+      <div className='container w-50'>
+        <h1 className='text-center mb-5'>Calculator</h1>
+        <div id='display' className='display bg-light border p-2 mb-2'>
+          {display}
         </div>
-        <div
-          id='operators'
-          className='w-50  d-flex flex-column align-items-center'
-        >
-          <button
-            id='clear'
-            className='btn btn-danger btn w-50  m-2'
-            onClick={clear}
+        <div className='d-flex flex  align-items-center justify-content-center h-100 border'>
+          <div id='numbers' className='d-flex flex-wrap justify-content-center'>
+            <button
+              id='seven'
+              className='btn btn-secondary btn-lg w-25  m-2'
+              onClick={handleNumber}
+            >
+              7
+            </button>
+            <button id='eight' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
+              8
+            </button>
+            <button id='nine' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
+              9
+            </button>
+            <button id='four' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
+              4
+            </button>
+            <button id='five' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
+              5
+            </button>
+            <button id='six' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
+              6
+            </button>
+            <button id='one' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
+              1
+            </button>
+            <button id='two' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
+              2
+            </button>
+            <button id='three' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
+              3
+            </button>
+            <button id='zero' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
+              0
+            </button>
+            <button id='decimal' className='btn btn-secondary btn-lg w-25  m-2' onClick={handleNumber}>
+              .
+            </button>
+            <button id='equals' className='btn btn-success btn-lg w-25  m-2' onClick={handleNumber}>
+              =
+            </button>
+          </div>
+          <div
+            id='operators'
+            className='w-75  d-flex flex-column align-items-center'
           >
-            AC
-          </button>
-          <button
-            id='divide'
-            className='btn btn-primary btn w-50  m-2'
-            onClick={() => handleOperatorClick('/')}
-          >
-            /
-          </button>
-          <button
-            id='multiply'
-            className='btn btn-primary btn w-50  m-2'
-            onClick={() => handleOperatorClick('x')}
-          >
-            x
-          </button>
-          <button
-            id='subtract'
-            className='btn btn-primary btn w-50  m-2'
-            onClick={() => handleOperatorClick('-')}
-          >
-            -
-          </button>
-          <button
-            id='add'
-            className='btn btn-primary btn w-50  m-2'
-            onClick={() => handleOperatorClick('+')}
-          >
-            +
-          </button>
+            <button id='clear' className='btn btn-danger btn w-50 m-2'>
+              AC
+            </button>
+            <button id='divide' className='btn btn-primary btn w-50  m-2'>
+              /
+            </button>
+            <button id='multiply' className='btn btn-primary btn w-50  m-2'>
+              x
+            </button>
+            <button id='subtract' className='btn btn-primary btn w-50  m-2'>
+              -
+            </button>
+            <button id='add' className='btn btn-primary btn w-50  m-2'>
+              +
+            </button>
+          </div>
         </div>
       </div>
     </div>
